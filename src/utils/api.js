33ffbde78221b04dee97169e1dd9be26d7b04f20
Shikhar Gupta <sgupta.9519@gmail.com>
2024-02-03 16:06:32 +0530

    initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": 'f624cc8c81msh44f6fd832a3e76fp13b1eajsn28a2f6bd04c9',
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
